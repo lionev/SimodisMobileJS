04bc6b05f39d3b743505cc9152eb572da1cbaec7
Julio <julionev10@gmail.com>
2021-11-29 16:37:18 -0300

    Fourth Commit

--- src/helpers/SimodisAPI.js
+++ src/helpers/SimodisAPI.js
@@ -1,61 +1,64 @@
-import {myToken} from './AuthHandler';
+
+import {  myToken } from './AuthHandler';
 
 export const signin = async (email, password) => {
-  const req = await fetch('https://backend-simodis.herokuapp.com/user/signin', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      email,
-      password,
-    }),
-  });
+    const req = await fetch('https://backend-simodis.herokuapp.com/user/signin', {
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email,
+        password
+      })
+    });
 
-  const json = await req.json();
-  return json;
-};
+    const json = await req.json();
+    return json;
+}
 
-export const signup = async (name, email, password) => {
-  const req = await fetch('https://backend-simodis.herokuapp.com/user/signup', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      name,
-      email,
-      password,
-    }),
-  });
+export const signup = async (name, email, password, isAdmin) => {
+    const req = await fetch('https://backend-simodis.herokuapp.com/user/signup', {
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+	isAdmin
+      })
+    });
 
-  const json = await req.json();
-  return json;
-};
+    const json = await req.json();
+    return json;
+}
 
 export const listcourse = async () => {
-  const req = await fetch('https://backend-simodis.herokuapp.com/course/list');
+  const req = await fetch("https://backend-simodis.herokuapp.com/course/list");
   const json = await req.json();
 
   return json.coursers;
-};
+}
 
 export const infoUser = async () => {
   const token = await myToken();
-  const req = await fetch(
-    'https://backend-simodis.herokuapp.com/user/info?token=' + `${token}`,
-  );
+  const req = await fetch('https://backend-simodis.herokuapp.com/user/info?token='+`${token}`);
 
-  const json = await req.json();
+  const json = await req.json();  
   return json;
-};
+}
 
 export const myCourse = async () => {
   const token = await myToken();
-  const req = await fetch(
-    'https://backend-simodis.herokuapp.com/course/mycourse?token=' + `${token}`,
-  );
+  const req = await fetch('https://backend-simodis.herokuapp.com/course/mycourse?token='+`${token}`);
 
-  const json = await req.json();
-  return json.mycourse;
-};
+  const json = await req.json();  
+  return json.coursersAndGrades;
+}
+
+
+
+
+
--- src/screams/Perfil.js
+++ src/screams/Perfil.js
@@ -1,99 +1,108 @@
-import React, {useEffect, useState} from 'react';
-import {View, Text, Image, Button} from 'react-native';
-import {infoUser, myCourse} from '../helpers/SimodisAPI';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image, Button } from 'react-native';
+import { infoUser, myCourse } from '../helpers/SimodisAPI';
 import styles from '../components/stylePerfil';
-import {Picker} from '@react-native-picker/picker';
+import {Picker } from '@react-native-picker/picker';
+
+
+
 
 const Screen = ({navigation}) => {
-  const [info, setInfo] = useState({});
-  const [cursos, setCursos] = useState([]);
-  const [selectedCourse, setSelectedCourse] = useState();
 
-  useEffect(() => {
-    const myCoursers = async () => {
-      try {
-        const json = await myCourse();
+    const [info, setInfo] = useState({}); 
+    const [cursos, setCursos] = useState([]);   
+    const [selectedCourse, setSelectedCourse] = useState();  
 
-        if (json) {
-          setCursos(json);
-        }
-      } catch (err) {
-        console.error(err);
-      }
-    };
+    useEffect(()=>{
+        const myCoursers = async () => {
+            try{
+                const json = await myCourse();
+            
+                if(json){                    
+                    setCursos(json);
+                    console.log(json);
+                }   
+            } catch(err){
+                console.error(err);
+            }            
+        }                   
+          
+        const infoUse = async () => {
+            try{
+                const json = await infoUser();
+            
+                if(json){
+                    setInfo(json);
+                }    
+            }catch (err){
+                console.error(err);
+            }                           
+        }  
+        myCoursers();
+        infoUse();                                                                     
+    }, []);            
 
-    const infoUse = async () => {
-      try {
-        const json = await infoUser();
-
-        if (json) {
-          setInfo(json);
-        }
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    myCoursers();
-    infoUse();
-  }, []);
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Image
-          style={styles.image}
-          source={require('../imagens/17004.png')}
-          resizeMode="contain"
-        />
-      </View>
-      {cursos.length !== 0 ? (
-        <View style={styles.body2}>
-          <Text style={styles.text}>Name: {info.name}</Text>
-          <Text style={styles.text}>Matricula: {info.enrollment}</Text>
-          <Text style={styles.text}>Email: {info.email}</Text>
-
-          <Picker
-            style={styles.picker}
-            selectedValue={selectedCourse}
-            onValueChange={(itemValue, itemIndex) =>
-              setSelectedCourse(itemValue)
-            }>
-            {cursos.map((item, index) => {
-              return (
-                <Picker.Item
-                  style={styles.text}
-                  label={item.course}
-                  value={item}
-                  key={index}
+    return(
+        <View style={styles.container}>
+            <View style={styles.header}>                
+                <Image 
+                    style={styles.image}
+                    source={require('../imagens/17004.png')}
+                    resizeMode="contain"
                 />
-              );
-            })}
-          </Picker>
+            </View> 
+            {
+                cursos.length !== 0 ?
+                <View style={styles.body2}>               
+                    <Text style={styles.text}>Name: {info.name}</Text>
+                    <Text style={styles.text}>Matricula: {info.enrollment}</Text>
+                    <Text style={styles.text}>Email: {info.email}</Text>                                                                                                    
+                    
+                    <Picker
+                        style={styles.picker}                         
+                        selectedValue={selectedCourse}
+                        onValueChange={(itemValue, itemIndex) => setSelectedCourse(itemValue)}                                
+                    >        
+                        {
+                           cursos.map((item, index) => { 
+                               return(                                                       
+                                    <Picker.Item style={styles.text} label={item.course} value={item} key={index} />                                                                                                                                    
+                               );
+                           }) 
+                        }                                                                                                                                                                                    
+                    </Picker> 
 
-          {selectedCourse ? (
-            <Text style={styles.text1}>Nota:{selectedCourse.grades}</Text>
-          ) : null}
+                    {
+                        selectedCourse ?                    
+                                                                                           
+                            <Text style={styles.text}>Nota:{selectedCourse.grades}</Text>                        
+
+                            :
+
+                            null
+
+                    }                                                                                                                               
+                                                           
+                                                                                                                                                    
+                </View>    
+
+                :
+
+                <View style={styles.body1}>               
+                    <Text style={styles.text}>Name: {info.name}</Text>
+                    <Text style={styles.text}>Matricula: {info.enrollment}</Text>
+                    <Text style={styles.text}>Email: {info.email}</Text>                                                                                
+                </View>    
+            }                            
+                                       
         </View>
-      ) : (
-        <View style={styles.body1}>
-          <Text style={styles.text}>Name: {info.name}</Text>
-          <Text style={styles.text}>Matricula: {info.enrollment}</Text>
-          <Text style={styles.text}>Email: {info.email}</Text>
-        </View>
-      )}
-    </View>
-  );
-};
+    );
+}
 
 Screen.navigationOptions = ({navigation}) => {
-  return {
-    headerRight: () => (
-      <Button
-        title="Cursos"
-        onPress={() => navigation.navigate('ListarProdutos')}
-      />
-    ),
-  };
-};
+    return {
+        headerRight: () => <Button title="Cursos" onPress={() => navigation.navigate('ListarProdutos')} />
+    }
+}
 
 export default Screen;
